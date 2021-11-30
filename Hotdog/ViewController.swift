63034a3618cc604567d3ad365836ea79e618471c
Jinyu Wang <ellie5201314@gmail.com>
2021-11-29 21:03:25 -0500

    class exist

--- Hotdog/ViewController.swift
+++ Hotdog/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Delegation design pattern -> Text field: Properties
+        
+    // Steps to implement a delegate:
+    // 1. Create an object
+    
+    let imagePicker = UIImagePickerController()
+    let results : [VNClassificationObservation] = []
+    
+    
+    
 
+    //2. Initialize the delegate in View Did Load
+    //3. Implement its functions
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    func detect(){
+        
+    }
 }
 
