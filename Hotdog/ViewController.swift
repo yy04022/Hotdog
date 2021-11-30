0db59b44c1064a7f0d51c144bbba99baf57e2492
Jinyu Wang <ellie5201314@gmail.com>
2021-11-29 20:06:40 -0500

    Initial Commit

---
+++ Hotdog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotdog
+//
+//  Created by Yu Jin on 11/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
